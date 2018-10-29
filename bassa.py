a63909e08e172c24dcf0c6f34dffdad75da9e546
Craig Cornelius <ccornelius@rodete-desktop-imager.corp.google.com>
2018-10-29 13:13:22 -0700

    Bassa in Bassa Vah script

---
+++ bassa.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import base
+
+# import transliterate
+# import transrule_ccp
+
+import json
+import logging
+import os
+import sys
+import urllib
+import webapp2
+
+from google.appengine.ext.webapp import template
+
+encoding_font_list = [
+    # {
+    #   'font_path':'/fonts/Bassa Gonya Leyka Noories.ttf',
+    #   'font_name':'BassaGonyaLeykaNoories',
+    #   'display_name': 'Gonya Leyka Noories',
+    #   'Source location': 'http://fontlibrary.org/en/font/rohingya-gonya-leyka-noories',
+    # },
+]
+
+unicode_font_list = [
+  {
+      'family': 'NotoSansBassaVah',
+      'longName': 'Noto Sans Bassa Vah',
+      'source': '/fonts/NotoSansBassaVah-Regular.ttf',
+  },
+]
+
+links = [
+    {'linkText': 'Keyboard',
+     'ref': '/bsq/'
+    },
+    {'linkText': 'Converter',
+     'ref': '/bsq/convertUI/'},
+    {'linkText': 'Font conversion summary',
+      'ref': '/bsq/encodingRules/'
+    },
+    {'linkText': 'Resources',
+      'ref': '/bsq/downloads/'
+    },
+    {'linkText': 'Unicode Page',
+     'ref': 'https://www.unicode.org/charts/PDF/U16AD0'
+  },
+    {'linkText': 'Language Wikipedia',
+     'ref': 'https://en.wikipedia.org/wiki/Bassa_language'
+    },
+
+    {'linkText': 'Combiners',
+     'ref': '/bsq/diacritic/'
+    },
+]
+
+class langInfo():
+  def __init__(self):
+    self.LanguageCode = 'bsq'
+    self.Language = 'Bassa'
+    self.Language_native = u'\u16AE2 ??'
+    self.direction = 'ltr'
+
+    if sys.maxunicode >= 0x10000:
+      logging.info('WIDE SYSTEM BUILD!!!')
+      self.diacritic_list = [unichr(x) for x in range(0x16af0, 0x16af4)]
+    else:
+      logging.info('NARROW SYSTEM BUILD!!!')
+      self.diacritic_list = [unichr(0xd803) + unichr(0xdd00 + x) for x in range(0x22, 0x27)]
+
+    self.base_consonant = u'\ud803\udd01'
+    self.baseHexUTF16 = u'\ud803\udd01'
+
+    self.lang_list = [
+      { 'shortName': self.LanguageCode,
+        'longName': self.Language,
+        }
+    ]
+    self.encoding_font_list = encoding_font_list
+    self.kb_list = [
+      {
+        'shortName': self.LanguageCode,
+        'longName': 'Bassa',
+        'jsName': self.LanguageCode,
+        'instructions': None,
+        'font': 'NotoSansBassaVah',
+      },
+    ]
+    self.links = links
+    self.text_file_list = []
+    self.unicode_font_list = unicode_font_list
+
+    # Lists of test characters for the various encodings
+    self.test_chars = [' '.join([unichr(x) for x in range(0x16ad0, 0x161e6)])]
+
+# Global in this file.
+langInstance = langInfo()
+
+app = webapp2.WSGIApplication(
+    [('/bsq/', base.LanguagesHomeHandler),
+     ('/bsq/convertUI/', base.ConvertUIHandler),
+     ('/bsq/downloads/', base.Downloads),
+     ('/bsq/converter/', base.ConvertHandler),
+     ('/bsq/encodingRules/', base.EncodingRules),
+     ('/bsq/diacritic/', base.DiacriticHandler),
+    ], debug=True,
+    config={'langInfo': langInstance}
+)
---
+++ layouts/bsq.js
@@ -0,0 +1,54 @@
+//javascript/keyboard/api/layouts/cpp.js
+// Copyright 2017 Google Inc.
+// All Rights Reserved.
+
+// Prototype for Bassa, 29-Oct-2018
+var BSQ_LAYOUT = {
+  'id': 'BSQ',
+  'title': 'Bassa in Bassa Vah - Work in progress',
+  'mappings': {
+    ',c': {
+      '': '{{\ud803\udd21}}{{\ud803\udd31}}{{\ud803\udd32}}{{\ud803\udd33}}{{\ud803\udd34}}' +
+	        '{{\ud803\udd35}}{{\ud803\udd36}}{{\ud803\udd37}}{{\ud803\udd38}}' +
+	        '{{\ud803\udd39}}{{\ud803\udd30}}-=' +
+          '{{\uD803\uDD0B}}{{\uD803\uDD0d}}{{\uD803\uDD02}}{{\uD803\uDD1a}}' +
+	        '{{\uD803\uDD09}}{{\uD803\uDD12}}{{\uD803\uDD18}}{{\uD803\uDD07}}' +
+	        '{{\uD803\uDD08}}{{\uD803\uDD06}}{{\uD803\uDD05}}{{\uD803\uDD0a}}\\' +
+          '{{\uD803\uDD10}}{{\uD803\uDD0f}}{{\uD803\uDD18}}{{\uD803\uDD01}}' +
+	        '{{\uD803\uDD13}}{{\uD803\uDD00}}{{\uD803\uDD07}}{{\uD803\uDD15}}' +
+	        '{{\uD803\uDD1d}}{{\uD803\uDD11}}{{\uD803\uDD04}}' +
+          '{{\uD803\uDD22}}{{\uD803\uDD20}}{{\uD803\uDD1e}}{{\uD803\uDD0c}}' +
+	        '{{\uD803\uDD1d}}{{\uD803\uDD1f\u065c}}{{\uD803\uDD03}}{{\uD803\uDD16}}' +
+            '{{\uD803\uDD0e}}{{\uD803\uDD09}}'
+    },
+    's,sc': {
+      '': '{{\ud803\udd1f}}\u0021\u0040\u0023\u0024\u0025^&' +
+            '\u002a\u0028\u0029\u005f\u003b' +
+          '{{\uD803\uDD26}}{{\uD803\uDD24}}{{\uD803\uDD1f}}{{\uD803\uDD23}}' +
+	        '{{}}{{}}{{}}{{}}' +
+	        '{{}}{{}}\u007b\u007d|' +
+          '{{\uD803\uDD14}}{{\uD803\uDD25}}{{\uD803\uDD23}}{{\uD803\uDD19}}' +
+            '{{\uD803\uDD1d}}{{}}{{}}{{}}' +
+            '{{}};\u0022' +
+          '{{}}{{}}{{}}{{}}' +
+            '{{}}{{}}{{}}<>?'
+    },
+    'l': {
+      '': '`1234567890-=' +
+          'qwertyuiop[]\\' +
+          'asdfghjkl;\'' +
+          'zxcvbnm,./'
+    },
+    'sl': {
+      '': '~!@#$%^&*()_+' +
+          'QWERTYUIOP{}|' +
+          'ASDFGHJKL:"' +
+          'ZXCVBNM<>?'
+    }
+  },
+  'transform': {
+  }
+}
+
+// Load the layout and inform the keyboard to switch layout if necessary.
+google.elements.keyboard.loadme(BSQ_LAYOUT);
