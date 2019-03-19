698adb4c1737f65fb770b2e1065f9adde5df4e25
Craig Cornelius <ccornelius@google.com>
2019-03-18 17:51:23 -0700

    Adding font-encoded Zaghawa (Beria) tools

---
+++ layouts/zag.js
@@ -0,0 +1,55 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Implements Ahom keyboard based on ...
+// TODO: Add reference
+
+var ZAG_LAYOUT = {
+  'id': 'zag',
+  'title': "Zaghawa Beria encoded",
+  'mappings': {
+    '': {
+      '': '`1234567890-=' +
+          'qwertyuiop[]\\' +
+          'asdfghjkl;\'' +
+          'zxcvbnm,./'
+    },
+    's, sc': {
+      '': '`1234567890-=' +
+          'QWERTYUIOP[]\\' +
+          'ASDFGHJKL;\'' +
+          'ZXCVBNM,./'
+    },
+    'c': {
+      '': '`1234567890-=' +
+          'qwertyuiop[]\\' +
+          'asdfghjkl;\'' +
+          'zxcvbnm,./'
+    },
+    'l,cl': {
+      '': '`1234567890-=' +
+          'QWERTYUIOP[]\\' +
+          'ASDFGHJKL;\'' +
+          'ZXCVBNM,./'
+    },
+    'sl,scl': {
+      '': '~!@#$%^&*()_+' +
+          'qwertyuiop{}|' +
+          'asdfghjkl:"' +
+          'zxcvbnm<>?'
+    }
+  },
+
+};
+
+// Load the layout and inform the keyboard to switch layout if necessary.
+google.elements.keyboard.loadme(ZAG_LAYOUT);
---
+++ zaghawa.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import base
+
+# import transliterate
+# import transrule_ccp
+
+import json
+import logging
+import os
+import sys
+import urllib
+import webapp2
+
+from google.appengine.ext.webapp import template
+
+encoding_font_list = [
+    {
+      'font_path':'/fonts/zawghawa/ZaghawaBeria.otf',
+      'font_name':'ZaghawaBeriaASCII',
+      'display_name': 'Zawhawa Beria ASCIi',
+      'Source location': 'https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=ZaghawaBeria_Hom/',
+    },
+]
+
+unicode_font_list = [
+#  {
+#      'family': 'RohingyaNooriesOne',
+#      'longName': 'Rohingya Noories One',
+#      'source': '/fonts/Rohingya Noories One.otf',
+#      'attribution': 'From MD Noor 25-Feb-2019',
+#  },
+]
+
+links = [
+    {'linkText': 'Keyboard',
+     'ref': '/zag/'
+    },
+    {'linkText': 'Converter',
+     'ref': '/zag/convertUI/'},
+    {'linkText': 'Font conversion summary',
+      'ref': '/zag/encodingRules/'
+    },
+#    {'linkText': 'Resources / Downloads',
+#      'ref': '/zae/downloads/'
+#    },
+#    {'linkText': 'Unicode Page',
+#    'ref': 'https://www.unicode.org/charts/PDF/???.pdf'
+#    },
+    {'linkText': 'Zaghawa Wikipedia',
+     'ref': 'https://en.wikipedia.org/wiki/Zaghawa_language'
+    },
+
+#    {'linkText': 'Combiners',
+#     'ref': '/rhg/diacritic/'
+#     },
+#    {'linkText': 'Keyboard layout suggestion',
+#     'ref': '',
+]
+
+class langInfo():
+  def __init__(self):
+    self.LanguageCode = 'zag'
+    self.Language = 'Zaghawa'
+    self.Language_native = ''
+    self.direction = 'rtl'
+
+    # NOT DEFINED YET
+    if sys.maxunicode >= 0x10000:
+      logging.info('WIDE SYSTEM BUILD!!!')
+      self.diacritic_list = [unichr(x) for x in range(0x10D22, 0x10D27)]
+    else:
+      logging.info('NARROW SYSTEM BUILD!!!')
+      self.diacritic_list = [unichr(0xd803) + unichr(0xdd00 + x) for x in range(0x22, 0x27)]
+
+    self.base_consonant = u'\ud803\udd01'
+    self.baseHexUTF16 = u'\ud803\udd01'
+
+    self.lang_list = [
+      { 'shortName': self.LanguageCode,
+        'longName': self.Language,
+        }
+    ]
+    self.encoding_font_list = encoding_font_list
+    self.kb_list = [
+      {
+        'shortName': self.LanguageCode,
+        'longName': 'Zaghawa Beria',
+        'jsName': self.LanguageCode,
+        'instructions': None,
+        'font': 'ZaghawaBeriaASCII',
+      },
+    ]
+    self.links = links
+    self.text_file_list = []
+    self.unicode_font_list = unicode_font_list
+
+    # Lists of test characters for the various encodings
+    self.test_chars = [' '.join([unichr(x) for x in range(0x620, 0x6f9)])]
+
+# Global in this file.
+langInstance = langInfo()
+
+app = webapp2.WSGIApplication(
+    [('/zag/', base.LanguagesHomeHandler),
+     ('/zag/convertUI/', base.ConvertUIHandler),
+     ('/zag/downloads/', base.Downloads),
+     ('/zag/converter/', base.ConvertHandler),
+     ('/zag/encodingRules/', base.EncodingRules),
+     ('/zag/diacritic/', base.DiacriticHandler),
+    ], debug=True,
+    config={'langInfo': langInstance}
+)
