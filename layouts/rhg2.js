8e6299104a467b1a0215b8e1651e4c16f784d74a
Craig Cornelius <cwcornelius@gmail.com>
2019-04-09 19:22:09 -0700

    Rohingya keyboard extended to doubled letters

--- layouts/rhg2.js
+++ layouts/rhg2.js
@@ -2,10 +2,13 @@
 // Copyright 2017 Google Inc.
 // All Rights Reserved.
 
-// Prototype for Chakma, 3-May-2017
+// Arabic punctuation marks?  Comma (60c), semicolon (61b), question mark (61f), full stop (6d4)
+// also double angle quotation 0xab 0xbb
+// Doubled vowels or other vowel combos?
+// Prototype for Rohingya, April 2019
 var RHG_LAYOUT2 = {
   'id': 'rhg2',
-  'title': 'Rohingya Hanifi layout 2',
+  'title': 'Rohingya',
   'mappings': {
     ',c': {
       '': '{{\ud803\udd22}}{{\ud803\udd31}}{{\ud803\udd32}}{{\ud803\udd33}}{{\ud803\udd34}}' +
@@ -47,19 +50,35 @@ var RHG_LAYOUT2 = {
     }
   },
   'transform': {
-    // consonants, independent vowels, and E
-    // Doubled ekaara
-    '\uD803\uDD2c\uD803\uDD2c\uD803([\uDD03-\uDD26])': '\uD803$1\uD803\uDD2c\uD803\uDD2c',
-    '\uD803\uDD2c\uD803([\uDD03-\uDD26])': '\uD803$1\uD803\uDD2c',
-    // Move E after the virama/consonant. \u001d marks the end point of a previous output.
-    '\uD803\uDD2c\uD803\uDD2c\u001d\uD803\uDD33\uD803([\uDD03-\uDD26])': '\uD803\uDD33\uD803$1\uD803\uDD2c\uD803\uDD2c',
-    '\uD803\uDD2c\u001d\uD803\uDD33\uD803([\uDD03-\uDD26])': '\uD803\uDD33\uD803$1\uD803\uDD2c',
-    // Reorder upper vs. lower diacritics
-    '\ud803([\udd2a\udd2b\udd31\udd32])\ud803([\udd27-\udd29\udd2d\udd30\udd34])':
-      '\ud803$2\ud803$1',
-    // Question: Should we convert doubled signs to the separate Unicode values?
-    // e.g., 11127 11127 -> 11127 11134?
-    '\uD803\uDD27\uD803\udd27': '\uD803\udd27\uD803\uDD34',
+    // doubled consonants to consonant + gemination
+    '\uD803(\uDD01)\uD803\uDD01': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD02)\uD803\uDD02': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD03)\uD803\uDD03': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD04)\uD803\uDD04': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD05)\uD803\uDD05': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD06)\uD803\uDD06': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD07)\uD803\uDD07': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD08)\uD803\uDD08': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD09)\uD803\uDD09': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD0a)\uD803\uDD0a': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD0b)\uD803\uDD0b': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD0c)\uD803\uDD0c': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD0d)\uD803\uDD0d': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD0e)\uD803\uDD0e': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD0f)\uD803\uDD0f': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD10)\uD803\uDD10': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD11)\uD803\uDD11': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD12)\uD803\uDD12': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD13)\uD803\uDD13': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD14)\uD803\uDD14': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD15)\uD803\uDD15': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD16)\uD803\uDD16': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD17)\uD803\uDD17': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD18)\uD803\uDD18': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD19)\uD803\uDD19': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD1a)\uD803\uDD1a': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD1b)\uD803\uDD1b': '\uD803$1\uD803\uDD27',
+    '\uD803(\uDD1c)\uD803\uDD1c': '\uD803$1\uD803\uDD27',
   }
 };
 
