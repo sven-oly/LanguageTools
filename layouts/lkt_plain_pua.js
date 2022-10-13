3321208f376a6067b573934c763f9cb4155df33d
Craig Cornelius <cwcornelius@gmail.com>
2022-10-12 21:15:50 -0700

    Lakota Plains font

---
+++ layouts/lkt_plain_pua.js
@@ -0,0 +1,52 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+var LKT_PLAIN_PUA_LAYOUT = {
+  'id': 'lkt_plain_pua',
+  'title': 'Lakota Plain PUA',
+  'mappings': {
+    ',c': {
+      '': '`1234567890-=' +
+          'qwertyuiop[]\\' +
+          '{{\ue761}}asdfghjkl;\'' +
+          'zxcvbnm,./'
+    },
+    's,sc': {
+      '': '~!@#$%^&*()_+' +
+          'QWERTYUIOP{}|' +
+          'ASDFGHJKL:"' +
+          'ZXCVBNM<>?'
+    },
+    'l,cl': {
+      '': '`1234567890-=' +
+          'qwertyuiop[]\\' +
+          'asdfghjkl;\'' +
+          'zxcvbnm,./'
+    },
+    'sl,scl': {
+      '': '~!@#$%^&*()_+' +
+          'QWERTYUIOP{}|' +
+          'ASDFGHJKL:"' +
+          'ZXCVBNM<>?'
+    }
+  },
+  'transform': {
+    '^': '^',  // Placeholder
+  }
+};
+
+// Load the layout and inform the keyboard to switch layout if necessary.
+google.elements.keyboard.loadme(LKT_PLAIN_PUA_LAYOUT);
+lkt_plain_pua = LKT_PLAIN_PUA_LAYOUT;
+
+
