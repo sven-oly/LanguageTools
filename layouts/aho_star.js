18f041d92c834558d69de89716feb6147bfb39b4
Craig Cornelius <cwcornelius@gmail.com>
2023-12-21 10:02:34 -0800

    Ahom Star updated with 7 new characters

--- layouts/aho_star.js
+++ layouts/aho_star.js
@@ -21,26 +21,21 @@ var AHO_STAR_LAYOUT = {
           '':
          '{{\ud805\udf3E}}{{\ud805\udf31}}{{\ud805\udf32}}{{\ud805\udf33}}{{\ud805\udf34}}{{\ud805\udf35}}{{\ud805\udf36}}{{\ud805\udf37}}{{\ud805\udf38}}{{\ud805\udf39}}{{\ud805\udf30}}\u002D\u003D{{\ud805\udf2B}}{{\ud805\udf1A}}{{\u200c\ud805\udf26}}{{\ud805\udf0D}}{{\ud805\udf04}}{{\ud805\udf0A}}{{\ud805\udf24}}{{\ud805\udf22}}{{\ud805\udf28}}{{\ud805\udf06}}\u005B\u005D\u005C{{\ud805\udf21}}{{\ud805\udf0F}}{{\ud805\udf13}}{{\ud805\udf02}}{{\ud805\udf15}}{{\ud805\udf11}}{{\ud805\udf16}}{{\ud805\udf00}}{{\ud805\udf0E}}\u003B\u0027{{\ud805\udf3C}}{{\ud805\udf05}}{{\ud805\udf0B}}{{\ud805\udf1A}}{{\ud805\udf08}}{{\ud805\udf03}}{{\ud805\udf09}}\u002C\u002E\u002F'
       },
-      's,sc': {
+      's': {
           '':
-          '\u007E\u0021\u0040\u0023\u0024\u0025\u005E\u0026\u002A\u0028\u0029\u005F\u002B{{\ud805\udf29}}{{\ud805\udf3B}}{{\ud805\udf1F}}{{\ud805\udf1E}}{{\ud805\udf0C}}{{}}{{\ud805\udf25}}{{\ud805\udf23}}{{\ud805\udf27}}{{\ud805\udf07}}\u007B\u007D\u007C{{\ud805\udf12}}{{}}{{\ud805\udf14}}{{}}{{\ud805\udf17}}{{\ud805\udf20}}{{\ud805\udf19}}{{\ud805\udf01}}{{\u200c\ud805\udf1D}}\u003A\u0022{{\ud805\udf3D}}{{\ud805\udf3A}}{{}}{{\ud805\udf3F}}{{\ud805\udf18}}{{\ud805\udf10}}{{\ud803\udf3a}}\u003C\u003E\u003F'
+          '\u007E\u0021\u0040{{\ud805\udf41}}{{\ud805\udf42}}\u0025{{\ud805\udf45}}\u0026\u002A\u0028\u0029\u005F\u002B{{\ud805\udf29}}{{\ud805\udf3B}}{{\ud805\udf1F}}{{\ud805\udf1E}}{{\ud805\udf0C}}{{}}{{\ud805\udf25}}{{\ud805\udf23}}{{\ud805\udf27}}{{\ud805\udf07}}\u007B\u007D\{{\ud805\udf26\ud805\udf21}}{{\ud805\udf12}}{{\ud805\udf46}}{{\ud805\udf14}}{{}}{{\ud805\udf17}}{{\ud805\udf20}}{{\ud805\udf19}}{{\ud805\udf01}}{{\u200c\ud805\udf1D}}\u003A\u0022{{\ud805\udf3D}}{{\ud805\udf3A}}{{\ud805\udf40}}{{\ud805\udf3F}}{{\ud805\udf18}}{{\ud805\udf10}}{{\ud803\udf3a}}{{\ud805\udf43}}{{\ud805\udf44}}\u003F'
       },
-    'c': {
+    'c,l,cl': {
       '': '`1234567890-=' +
           'qwertyuiop[]\\' +
           'asdfghjkl;\'' +
           'zxcvbnm,./'
-    },    'l,cl': {
-      '': '`1234567890-=' +
+    },
+      'sc,sl,scl': {
+      '': '~!@#$%^&*()_+' +
           'QWERTYUIOP[]\\' +
           'ASDFGHJKL;\'' +
           'ZXCVBNM,./'
-    },
-    'sl,scl': {
-      '': '~!@#$%^&*()_+' +
-          'qwertyuiop{}|' +
-          'asdfghjkl:"' +
-          'zxcvbnm<>?'
     }
   },
   'transform' : {
